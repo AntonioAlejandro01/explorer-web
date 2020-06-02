bba8eaff3496aef82b9399f0daa51ba6ad97f6db
AntonioAlejandro01 <antonioalejandroserranoramirez@gmail.com>
2020-06-02 19:57:10 +0200

    fix service

--- src/app/chest/chest.module.ts
+++ src/app/chest/chest.module.ts
@@ -5,15 +5,8 @@ import { ChestRoutingModule } from './chest-routing.module';
 import { ChestComponent } from './components/chest/chest.component';
 import { MaterialModule } from '../material/material.module';
 import { SharedModule } from '../shared/shared.module';
-import { CoreModule } from 'src/app/core/core.module';
 @NgModule({
   declarations: [ChestComponent],
-  imports: [
-    CommonModule,
-    ChestRoutingModule,
-    MaterialModule,
-    SharedModule,
-    CoreModule
-  ]
+  imports: [CommonModule, ChestRoutingModule, MaterialModule, SharedModule],
 })
-export class ChestModule { }
+export class ChestModule {}
